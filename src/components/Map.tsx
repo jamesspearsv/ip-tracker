78121616073e226d5982c40b4f23b4ab3104477c
James Spears <jamesspearsv@gmail.com>
2025-01-10 21:42:02 -0500

    Add main hero and update package.json

--- src/components/Map.tsx
+++
@@ -1,6 +0,0 @@
-import * as L from 'leaflet';
-
-export default function MapContainer() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
-  return <div id="map" style={{ height: '250px' }}></div>;
-}
