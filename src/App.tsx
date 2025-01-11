e2ac1e13fe96ab4db66eb26772c453cab34df7a7
James Spears <jamesspearsv@gmail.com>
2025-01-10 21:42:25 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Hero from './components/Hero';
 import InfoDisplay from './components/InfoDisplay';
-import MapContainer from './components/Map';
 
 export default function App() {
   return (
     <main>
       <Hero />
       <InfoDisplay />
-      <MapContainer />
     </main>
   );
 }
